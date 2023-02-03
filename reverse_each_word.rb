b25f9985b2dfb5e1f6c0626af8737b299eb5854c
Joshua David Jennings <jennings.josh@gmail.com>
2023-02-02 19:05:00 -0600

    finished

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,19 @@
-# Write your code here
+require 'pry'
+
+# def reverse_each_word(sentence)
+#   words = sentence.split
+#   reversed_words = []
+
+#   words.each do |word|
+#     reversed_words << word.reverse
+#   end
+
+#   reversed_words.join(" ")
+# end
+
+# below is the  Proc shorthand syntax and method chaining:
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+  end
+
+puts reverse_each_word("Hello there, and how are you?")
